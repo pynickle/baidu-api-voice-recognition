3878247fdd5fd933a9c8ed5debc232d067cc0aca
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-07-13 22:26:09 +0800

    update first

---
+++ index.py
@@ -0,0 +1,64 @@
+import os
+from aip import AipSpeech
+import cut_wav
+import wav_to_pcm
+import combine_txt
+
+
+DIR = [".\\video", ".\\output", ".\\text"]
+for i in DIR:
+    if not os.path.exists(i):
+        os.mkdir(i)
+    else:
+        os.system("rmdir /S /Q " + i)
+        os.mkdir(i)
+
+cut_wav.main()
+print("切割wav文件完成！即将进入wav转pcm！")
+os.system("pause")
+print("\n" * 10)
+
+wav_to_pcm.main()
+print("wav文件转换pcm完成！即将进入请求api！")
+os.system("pause")
+print("\n" * 10)
+
+files = os.listdir(".\\output")
+files_num = len(files)
+
+APP_ID = '16799063'
+API_KEY = 'pKZkOBI1Fc4HnelKsNwWDv1y'
+SECRET_KEY = 'dYr6yk1ApFCOdO06zh6xnHiBtohREb4q'
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
+
+for i in files:
+    print(".\\output\\" + i)
+    result = client.asr(get_file_content(".\\output\\" + i), 'pcm', 16000, {
+        'dev_pid': 1537,
+    })
+    print(result)
+    if result["err_no"] == 0:
+        with open(".\\text\\" + i[:-4] + ".txt", "a", encoding="utf-8") as f:
+            f.write(result["result"][0])
+    else:
+        with open(".\\text\\" + i[:-4] + ".txt", "a", encoding="utf-8") as f:
+            f.write(i[:-4] + "转换失败")
+
+print("识别并写入txt成功！即将进入合并！")
+os.system("pause")
+print("\n" * 10)
+
+combine_txt.main(files_num)
+"""
+result = client.asr(get_file_content(".\\16k.pcm"), 'pcm', 16000, {
+    'dev_pid': 1537,
+})
+print(result, type(result))
+"""
+
